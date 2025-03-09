5e7e48a7185805b776c6751d4373ba8ddd5ef175
ruziba3vich <rruzibayevich@gmail.com>
2025-03-09 13:26:47 +0500

    fix in first problem

--- n1.cpp
+++ n1.cpp
@@ -1,40 +1,36 @@
 #include <iostream>
 #include <string>
 
-std::string longestPalindrome(std::string s) {
-    if (s.empty()) return "";
-    int start = 0, maxLength = 1;
-    int n = s.length();
-    
-    for (int i = 0; i < n; i++) {
-        int left = i, right = i;
-        while (left >= 0 && right < n && s[left] == s[right]) {
-            if (right - left + 1 > maxLength) {
-                start = left;
-                maxLength = right - left + 1;
+class Solution {
+public:
+    std::string longestPalindrome(std::string s) {
+        if (s.empty()) return "";
+        int start = 0, maxLength = 1, n = s.length();
+
+        auto expandAroundCenter = [&](int left, int right) {
+            while (left >= 0 && right < n && s[left] == s[right]) {
+                if (right - left + 1 > maxLength) {
+                    start = left;
+                    maxLength = right - left + 1;
+                }
+                left--;
+                right++;
             }
-            left--;
-            right++;
+        };
+
+        for (int i = 0; i < n; i++) {
+            expandAroundCenter(i, i);
+            expandAroundCenter(i, i + 1);
         }
-        
-        left = i, right = i + 1;
-        while (left >= 0 && right < n && s[left] == s[right]) {
-            if (right - left + 1 > maxLength) {
-                start = left;
-                maxLength = right - left + 1;
-            }
-            left--;
-            right++;
-        }
+
+        return s.substr(start, maxLength);
     }
-    
-    return s.substr(start, maxLength);
-}
+};
 
 int main() {
+    Solution solution;
     std::string s;
     std::getline(std::cin, s);
-    std::string result = longestPalindrome(s);
-    std::cout << result << std::endl;
+    std::cout << solution.longestPalindrome(s) << std::endl;
     return 0;
 }
