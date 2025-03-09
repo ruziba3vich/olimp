a6905ea5a2657ac8bd177a5d366cf231a08db84f
ruziba3vich <rruzibayevich@gmail.com>
2025-03-09 13:25:18 +0500

    3Sum updated

--- n2.cpp
+++ n2.cpp
@@ -2,34 +2,47 @@
 #include <vector>
 #include <algorithm>
 
-std::vector<int> threeSum(std::vector<int>& nums) {
-    std::vector<int> result;
-    int n = nums.size();
-    if (n < 3) return result;
-    
-    std::sort(nums.begin(), nums.end());
-    
-    for (int i = 0; i < n - 2; i++) {
-        if (i > 0 && nums[i] == nums[i-1]) continue;
+class Solution {
+public:
+    std::vector<std::vector<int>> threeSum(std::vector<int>& nums) {
+        std::vector<std::vector<int>> result;
+        int n = nums.size();
+        if (n < 3) return result;
         
-        int left = i + 1;
-        int right = n - 1;
+        std::sort(nums.begin(), nums.end());
         
-        while (left < right) {
-            int sum = nums[i] + nums[left] + nums[right];
-            if (sum == 0) {
-                result = {nums[i], nums[left], nums[right]};
-                return result;
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+            int left = i + 1, right = n - 1;
+            
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[left], nums[right]});
+                    
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    
+                    left++;
+                    right--;
+                } 
+                else if (sum < 0) {
+                    left++;
+                } 
+                else {
+                    right--;
+                }
             }
-            else if (sum < 0) left++;
-            else right--;
         }
+        
+        return result;
     }
-    
-    return result;
-}
+};
 
 int main() {
+    Solution solution;
     int n;
     std::cout << "Enter number of elements: ";
     std::cin >> n;
@@ -40,16 +53,18 @@ int main() {
         std::cin >> nums[i];
     }
     
-    std::vector<int> result = threeSum(nums);
+    std::vector<std::vector<int>> results = solution.threeSum(nums);
     
-    if (result.empty()) {
-        std::cout << "No triplet found that sums to 0" << std::endl;
+    if (results.empty()) {
+        std::cout << "No triplets found that sum to 0" << std::endl;
     } else {
-        std::cout << "Triplet that sums to 0: ";
-        for (int num : result) {
-            std::cout << num << " ";
+        std::cout << "Triplets that sum to 0: " << std::endl;
+        for (const auto& triplet : results) {
+            for (int num : triplet) {
+                std::cout << num << " ";
+            }
+            std::cout << std::endl;
         }
-        std::cout << std::endl;
     }
     
     return 0;
